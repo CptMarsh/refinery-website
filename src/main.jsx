38f013c3bf0086a87060a8a4844b93b8cb309626
CptMarsh <schapiro.omer@gmail.com>
2026-03-09 13:28:46 +0200

    setting up a pure static website

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+const rootElement = document.getElementById('root')
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrateRoot(rootElement, <App />)
+} else {
+  ReactDOM.createRoot(rootElement).render(<App />)
+}
